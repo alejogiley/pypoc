82bc09f2bb0b49989033e9e6b8234fcd948dc152
alejogiley <alejogiley@gmail.com>
2019-07-25 12:22:58 -0400

    create variational autoencoder

--- examples/__functions.py
+++ examples/__functions.py
@@ -99,3 +99,25 @@ def wget_urls(url):
         print('File %s downloaded' % name)
     except:
         print('Error during download of %s' % url)
+
+# reparameterization trick
+# instead of sampling from Q(z|X), sample eps = N(0,I)
+# z = z_mean + sqrt(var)*eps
+from keras import backend as K
+
+def sampling(args):
+    """Reparameterization trick by sampling fr an isotropic unit Gaussian.
+
+    # Arguments
+        args (tensor): mean and log of variance of Q(z|X)
+
+    # Returns
+        z (tensor): sampled latent vector
+    """
+
+    z_mean, z_log_var = args
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+    # by default, random_normal has mean=0 and std=1.0
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
